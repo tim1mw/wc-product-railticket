d0bfe8716b0588af794a77ae4769aa64d41568d0
Tim Williams <tmw@autotrain.org>
2020-06-28 22:37:30 +0100

    Add to cart now working with full product display

---
+++ editlib.php
@@ -0,0 +1,37 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// Hook for adding admin menus
+add_action('admin_menu', 'railticket_add_pages');
+add_action('admin_init', 'railticket_register_settings' );
+
+
+function railticket_register_settings() {
+   add_option('wc_product_railticket_woocommerce_product', '');
+   register_setting('wc_product_railticket_options_main', 'wc_product_railticket_woocommerce_product'); 
+   register_setting('wc_product_railticket_options_main', 'wc_product_railticket_min_price'); 
+}
+
+function railticket_add_pages() {
+    add_submenu_page('railtimetable-top-level-handle', "Railticket", "Railticket", 'manage_options', 'railticket-options', 'railticket_options');
+}
+
+function railticket_options() {
+    ?>
+    <h1>Heritage Railway Tickets</h1>
+    <form method="post" action="options.php">
+    <?php settings_fields('wc_product_railticket_options_main'); ?>
+    <table>
+        <tr valign="top">
+            <th scope="row"><label for="wc_product_railticket_woocommerce_product">Woocommerce Product ID</label></th>
+            <td><input type="text" id="wc_product_railticket_woocommerce_product" name="wc_product_railticket_woocommerce_product" value="<?php echo get_option('wc_product_railticket_woocommerce_product'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><label for="wc_product_railticket_min_price">Minimum Ticket Order Price</label></th>
+            <td><input type="text" id="wc_product_railticket_min_price" name="wc_product_railticket_min_price" value="<?php echo get_option('wc_product_railticket_min_price'); ?>" /></td>
+        </tr>
+    </table>
+    <?php submit_button(); ?>
+    </form>
+    <?php
+}
