2dff87b3622f5bd6942ce9625017cb6c9302393b
Tim Williams <tmw@autotrain.org>
2021-04-25 19:23:08 +0100

    Currency formatting for the waybill

--- classes/Waybill.php
+++ classes/Waybill.php
@@ -120,7 +120,7 @@ class Waybill extends Report {
             } else {
                 $gti->name = __('Unknown User', 'wc_railticket')." (id = ".$createdby.")";
             }
-            $gti->total = $total;
+            $gti->total = number_format($total, 2);
             $gts[] = $gti;
         }
 
@@ -172,8 +172,8 @@ class Waybill extends Report {
             $nline[] = $linevalue;
 
             $fare = $this->bookableday->fares->get_fare($from, $to, $keyparts[2], $keyparts[3], $keyparts[4], $dtype, $special);
-            $nline[] = $fare;
-            $nline[] = $linevalue * $fare;
+            $nline[] = number_format($fare, 2);
+            $nline[] = number_format($linevalue * $fare, 2);
             fputcsv($f, $nline);
         }
 
@@ -228,7 +228,8 @@ class Waybill extends Report {
 
             $nline->number = $linevalue;
             $nline->fare = $this->bookableday->fares->get_fare($from, $to, $keyparts[2], $keyparts[3], $keyparts[4], $dtype, $special);
-            $nline->total = $linevalue * $nline->fare;
+            $nline->total = number_format($linevalue * $nline->fare, 2);
+            $nline->fare = number_format($nline->fare, 2);
             $plines[] = $nline;
         }
 
@@ -239,17 +240,17 @@ class Waybill extends Report {
 
         $alldata->lines = $plines;
 
-        $alldata->totalrevenue = $this->totalmanual + $this->totalwoo;
+        $alldata->totalrevenue = number_format($this->totalmanual + $this->totalwoo, 2);
         $alldata->totalseats = $this->totalseats;
         $alldata->totaltickets = $this->totaltickets;
         $alldata->totaljourneys = $this->totaljourneys;
         $alldata->guards = array_values($this->guardtotals);
-        $alldata->totalmanual = $this->totalmanual;
-        $alldata->totalwoo = $this->totalwoo;
-        $alldata->totalonlineprice = $this->totalonlineprice;
-        $alldata->totalguardprice = $this->totalguardprice;
-        $alldata->totaldiscounts = $this->totaldiscounts;
-        $alldata->totalsupplements = $this->totalsupplements;
+        $alldata->totalmanual = number_format($this->totalmanual, 2);
+        $alldata->totalwoo = number_format($this->totalwoo, 2);
+        $alldata->totalonlineprice = number_format($this->totalonlineprice, 2);
+        $alldata->totalguardprice = number_format($this->totalguardprice, 2);
+        $alldata->totaldiscounts = number_format($this->totaldiscounts, 2);
+        $alldata->totalsupplements = number_format($this->totalsupplements, 2);
 
         $alldata->guards = $gts;
 
